a6269e2768ec7e883d5e7f4347e5f30dba0dc987
Doug Stewart <contact@dougstwart.me>
2021-03-20 11:55:03 -0700

    :sparkles: First release

---
+++ precache-manifest.3262536ddd97898e4129f411d6e13d39.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6753e22b24b0555fb2afa21cb833150a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d33783810a340d9ac97",
+    "url": "/static/js/2.b4b9284e.chunk.js"
+  },
+  {
+    "revision": "2e39177caed1fd98896873a816491671",
+    "url": "/static/js/2.b4b9284e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89da0923376dc02208f1",
+    "url": "/static/js/main.06defe88.chunk.js"
+  },
+  {
+    "revision": "b4335971630111561d87",
+    "url": "/static/js/runtime-main.c3f81f18.js"
+  },
+  {
+    "revision": "c602ea5ac0d8608035937dc01a0a06b9",
+    "url": "/static/media/arrow.c602ea5a.svg"
+  },
+  {
+    "revision": "0a029773bd2eeecd6d0d968d2cc5b428",
+    "url": "/static/media/cancel.0a029773.svg"
+  },
+  {
+    "revision": "34d937c40f96c494567971086d23f4c6",
+    "url": "/static/media/letter-logo.34d937c4.svg"
+  },
+  {
+    "revision": "adcb3bcaab988591d9fbdde4147e9251",
+    "url": "/static/media/logout.adcb3bca.svg"
+  },
+  {
+    "revision": "5a2aa3e843af5245194377583ff7b9a0",
+    "url": "/static/media/move.5a2aa3e8.svg"
+  },
+  {
+    "revision": "b1990df616de0f26346d8d264e4c367b",
+    "url": "/static/media/plus.b1990df6.svg"
+  },
+  {
+    "revision": "8687fb00841b7ca049e09f57ce1e79c9",
+    "url": "/static/media/settings.8687fb00.svg"
+  }
+]);
